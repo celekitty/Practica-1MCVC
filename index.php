d064bfbbdd61b4aefc515f054dc2339ceadf4941
Maria Celeste Vanegas Cea <celesteuwu9@gmail.com>
2024-08-13 19:59:55 -0600

    Creacion de index.php con estructura html

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MARIA CELESTE VANEGAS CEA</title>    
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/style.css"/>
+    <script src = "js/jquery-3.7.1.min"></script>
+    <script>src ="js/bootstrap.bundle.min.js"</script>
+</head>
+
+</html>
